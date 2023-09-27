cf95ef321ba255f25ea9c907b8eeba3eac0e9335
bruhwarda <wbywardah@gmail.com>
2023-09-27 21:20:00 +0500

    card slider changed

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const slides = document.querySelectorAll(".slide");
-// console.log(slides);
 var counter = 0;
+
 slides.forEach((slide, index) => {
   slide.style.left = `${index * 100}%`;
 });
 
 const goPrev = () => {
   counter === 0 ? (counter = slides.length - 1) : counter--;
-
   slideCards();
 };
+
 const goNext = () => {
   counter === slides.length - 1 ? (counter = 0) : counter++;
   slideCards();
